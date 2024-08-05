f51d7165f7ee93efad0f7a151955371221e5bc60
baljindersingh01 <baljindersingh@Baljinders-Air.lan>
2024-08-05 19:15:34 +0530

    login functionality added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
