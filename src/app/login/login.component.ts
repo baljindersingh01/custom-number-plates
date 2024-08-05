3b8f61b49e9360ecbae932b654d589a30e02f659
baljindersingh01 <baljindersingh@Baljinders-Air.lan>
2024-08-05 19:12:15 +0530

    login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatButtonModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  constructor(private router: Router) {}
+  login(username: string, password: string) {
+    if (username == 'baljinder.s@samsung.com' && password == '10001') {
+      this.router.navigate(['/home']);
+    } else {
+      alert('You Have Entered Wrong Details...');
+    }
+  }
+}
